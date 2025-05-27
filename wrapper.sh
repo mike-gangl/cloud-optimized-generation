4ec800502b9b2f2aa2a54dcd663971b3b3b74235
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-05-27 12:06:28 -0700

    added better handling of null values

--- wrapper.sh
+++ wrapper.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Environment Variables must be passed into the docker run command
 
+[[ -z "${START_DATE}" ]] && startDate='None' || startDate="${START_DATE}"
+[[ -z "${END_DATE}" ]] && endDate='None' || endDate="${END_DATE}"
+
+
+
 #need to update this 
 export edl_username=`aws ssm get-parameter --with-decryption --name generate-edl-username | jq .Parameter.Value --raw-output`
 export edl_password=`aws ssm get-parameter --with-decryption --name generate-edl-password | jq .Parameter.Value --raw-output`
@@ -11,4 +16,4 @@ machine urs.earthdata.nasa.gov
   password $edl_password
 EOF
 
-papermill generate_cloud_optimized_store.ipynb output.ipynb -p collection $COLLECTION -p loadable_coord_vars $LOADABLE_VARS -p start_date = $START_DATE -p end_date $END_DATE
+papermill generate_cloud_optimized_store.ipynb output.ipynb --log-output  -p collection $COLLECTION -p loadable_coord_vars $LOADABLE_VARS -p start_date $startDate -p end_date $endDate
